a44dd9a531ae487cfbfbf54f6a158849777eb755
Rikhard Honkanen <rikhard.honkanen@learnet.se>
2021-02-19 10:02:50 +0100

    Day 1 Progress

--- ProjektarbeteButik/MainWindow.xaml.cs
+++ ProjektarbeteButik/MainWindow.xaml.cs
@@ -25,6 +25,8 @@ namespace ProjektarbeteButik
     public partial class MainWindow : Window
     {
         public Thickness spacing = new Thickness(5);
+        public StackPanel shopInventoryPanel;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -54,35 +56,55 @@ namespace ProjektarbeteButik
             mainGrid.ColumnDefinitions.Add(new ColumnDefinition());
 
             //Shop Inventory (Left side)
-            StackPanel shopInventory = new StackPanel
-            {
-                Orientation = Orientation.Vertical,
-                Margin = spacing                
-            };
+            StackPanel shopInventory = CreateShopInventoryPanel();
             mainGrid.Children.Add(shopInventory);
             Grid.SetColumn(shopInventory, 0);
             Grid.SetRow(shopInventory, 0);
             Grid.SetRowSpan(shopInventory, 2);
 
-            Label inventoryLabel = new Label
+            //Shopping Cart (Top Right side)
+            Grid shoppingCartGrid = CreateCartGrid();
+            mainGrid.Children.Add(shoppingCartGrid);
+            Grid.SetColumn(shoppingCartGrid, 1);
+            Grid.SetRow(shoppingCartGrid, 0);
+
+            //Check Out (Bottom Right side)
+            Grid checkOutGrid = CreateCheckOutGrid();
+            mainGrid.Children.Add(checkOutGrid);
+            Grid.SetColumn(checkOutGrid, 1);
+            Grid.SetRow(checkOutGrid, 1);            
+        }
+
+        public StackPanel CreateShopInventoryPanel()
+        {
+            shopInventoryPanel = new StackPanel
             {
                 Margin = spacing,
-                Content = "Shop Inventory",
-                HorizontalAlignment = HorizontalAlignment.Center,
-                FontSize = 18
+                Orientation = Orientation.Vertical,
+                Background = Brushes.OldLace
             };
-            shopInventory.Children.Add(inventoryLabel);
 
-            //Shopping Cart (Top Right side)
+            Label shopInventoryLabel = new Label
+            {
+                Content = "Shopping Inventory",
+                Margin = spacing,
+                FontSize = 18,
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
+            shopInventoryPanel.Children.Add(shopInventoryLabel);
+
+            AddProducts();
+
+            return shopInventoryPanel;
+        }
+        public Grid CreateCartGrid()
+        {
             Grid shoppingCartGrid = new Grid();
             shoppingCartGrid.Margin = spacing;
             shoppingCartGrid.RowDefinitions.Add(new RowDefinition());
             shoppingCartGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
             shoppingCartGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
             shoppingCartGrid.ColumnDefinitions.Add(new ColumnDefinition());
-            mainGrid.Children.Add(shoppingCartGrid);
-            Grid.SetColumn(shoppingCartGrid, 1);
-            Grid.SetRow(shoppingCartGrid, 0);
 
             StackPanel cartInventoryPanel = new StackPanel
             {
@@ -122,7 +144,10 @@ namespace ProjektarbeteButik
             Grid.SetColumn(cartTotalLabel, 0);
             Grid.SetRow(cartTotalLabel, 2);
 
-            //Check Out (Bottom Right side)
+            return shoppingCartGrid;
+        }
+        public Grid CreateCheckOutGrid()
+        {
             Grid checkOutGrid = new Grid();
             checkOutGrid.Margin = spacing;
             checkOutGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
@@ -130,9 +155,6 @@ namespace ProjektarbeteButik
             checkOutGrid.RowDefinitions.Add(new RowDefinition());
             checkOutGrid.ColumnDefinitions.Add(new ColumnDefinition());
             checkOutGrid.ColumnDefinitions.Add(new ColumnDefinition());
-            mainGrid.Children.Add(checkOutGrid);
-            Grid.SetColumn(checkOutGrid, 1);
-            Grid.SetRow(checkOutGrid, 1);
 
             TextBox couponTextBox = new TextBox
             {
@@ -153,12 +175,92 @@ namespace ProjektarbeteButik
             checkOutGrid.Children.Add(checkOutButton);
             Grid.SetColumn(checkOutButton, 1);
             Grid.SetRow(checkOutButton, 1);
+
+            return checkOutGrid;
         }
+
         public void AddProducts()
         {
             //read from CSV
             //For each item: 
             //Create horizontal stackpanel with Picture, name, descrip, prize & "Add to cart button"
+            Grid productGrid = new Grid();
+            productGrid.RowDefinitions.Add(new RowDefinition());
+            productGrid.RowDefinitions.Add(new RowDefinition());
+            productGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            productGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+            productGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+            shopInventoryPanel.Children.Add(productGrid);
+            Grid.SetColumn(productGrid, 0);
+            Grid.SetRow(productGrid, 1);
+
+            Button addToCart = new Button
+            {
+                Content = "Add To Cart",
+                Margin = spacing,
+                Padding = spacing
+            };
+            productGrid.Children.Add(addToCart);
+            Grid.SetRow(addToCart, 0);
+            Grid.SetColumn(addToCart, 2);
+
+            Label productLabel = new Label
+            {
+                Content = "Dodo-Bird",
+                FontSize = 12,
+            };
+            productGrid.Children.Add(productLabel);
+            Grid.SetRow(productLabel, 0);
+            Grid.SetColumn(productLabel, 0);
+
+            TextBox priceBox = new TextBox
+            {
+                Text = "Price",
+                FontSize = 12,
+                IsReadOnly = true,
+            };
+            productGrid.Children.Add(priceBox);
+            Grid.SetRow(priceBox, 1);
+            Grid.SetColumn(priceBox, 2);
+
+            Image productImage = CreateImage(@"Images\exampleimage2.webp");
+            var productImageResized = resizeImage(productImage, new Size(50, 50));
+            productImage.Stretch = Stretch.Fill;
+            productGrid.Children.Add(productImage);
+            Grid.SetRow(productImage, 1);
+            Grid.SetColumn(productImage, 0);
+        }        
+
+        public static Image resizeImage(Image imgToResize, Size size)
+        {
+            return (Image)(new Bitmap(imgToResize, size));
+        }
+
+        private Image CreateImage(string filePath)
+        {
+            ImageSource source = new BitmapImage(new Uri(filePath, UriKind.Relative));
+            Image image = new Image
+            {
+                Source = source,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Margin = new Thickness(5),
+            };
+            // A small rendering tweak to ensure maximum visual appeal.
+            RenderOptions.SetBitmapScalingMode(image, BitmapScalingMode.HighQuality);
+            return image;
+        }
+    }
+
+    internal class Bitmap : Image
+    {
+        private Image imgToResize;
+        private Size size;
+
+        public Bitmap(Image imgToResize, Size size)
+        {
+            this.imgToResize = imgToResize;
+            this.size = size;
         }
     }
 }
