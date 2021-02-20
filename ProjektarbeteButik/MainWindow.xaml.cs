7b9bb773b09d3994a668e304d7e69e81cc538002
Dan Strandberg <DanStrandberg_@hotmail.com>
2021-02-20 09:35:02 +0100

    Ignorera denna. Behöver bara pusha för att kunna pulla dina förändringar.

--- ProjektarbeteButik/MainWindow.xaml.cs
+++ ProjektarbeteButik/MainWindow.xaml.cs
@@ -18,6 +18,8 @@ namespace ProjektarbeteButik
     public partial class MainWindow : Window
     {
         public Thickness spacing = new Thickness(5);
+        public Label TextLabel;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -46,23 +48,113 @@ namespace ProjektarbeteButik
             mainGrid.ColumnDefinitions.Add(new ColumnDefinition());
             mainGrid.ColumnDefinitions.Add(new ColumnDefinition());
 
-            StackPanel shopInventory = new StackPanel
-            {
-                Margin = spacing               
-                
-            };
+            StackPanel shopInventory = CreateLeftPanel();
             mainGrid.Children.Add(shopInventory);
             Grid.SetColumn(shopInventory, 0);
             Grid.SetRow(shopInventory, 0);
             Grid.SetRowSpan(shopInventory, 2);
+        }
 
-            Label inventoryLabel = new Label
-            {
-                Content = "Shop Inventory",
-                HorizontalAlignment = HorizontalAlignment.Center,
-                FontSize = 18
+        public StackPanel CreateLeftPanel()
+        {
+            StackPanel shopInventory = new StackPanel 
+            { 
+                //Orientation = Orientation.Vertical,
+                Background = Brushes.OldLace,
             };
-            shopInventory.Children.Add(inventoryLabel);
+
+            TextLabel = new Label
+            {
+                Content = "Shopping Inventory",
+                Margin = spacing,
+                FontSize = 20,
+                HorizontalAlignment = HorizontalAlignment.Center,
+            };
+            shopInventory.Children.Add(TextLabel);
+
+            Grid productGrid = new Grid();
+            productGrid.RowDefinitions.Add(new RowDefinition());
+            productGrid.RowDefinitions.Add(new RowDefinition());
+            productGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            productGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+            //productGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            productGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+            shopInventory.Children.Add(productGrid);
+            Grid.SetColumn(productGrid, 0);
+            Grid.SetRow(productGrid, 1);
+
+            Button addToCart = new Button
+            {
+                Content = "Add To Cart",
+                Margin = spacing,
+                Padding = spacing
+            };
+            productGrid.Children.Add(addToCart);
+            Grid.SetRow(addToCart, 0);
+            Grid.SetColumn(addToCart, 2);
+
+            TextLabel = new Label
+            {
+                Content = "Dodo-Bird",
+                FontSize = 13,
+            };
+            productGrid.Children.Add(TextLabel);
+            Grid.SetRow(TextLabel, 0);
+            Grid.SetColumn(TextLabel, 0);
+
+            TextBox priceBox = new TextBox
+            {
+                Text = "Price",
+                FontSize = 13,
+                IsReadOnly = true,
+            };
+            productGrid.Children.Add(priceBox);
+            Grid.SetRow(priceBox, 1);
+            Grid.SetColumn(priceBox, 2);
+
+            Image productImage = CreateImage("nedladdning.jfif");
+            var productImageResized = resizeImage(productImage, new Size(50, 50));
+            productImage.Stretch = Stretch.Fill;
+            productGrid.Children.Add(productImage);
+            Grid.SetRow(productImage, 1);
+            Grid.SetColumn(productImage, 0);
+
+            
+
+            return shopInventory;
+        }
+        public static Image resizeImage(Image imgToResize, Size size)
+        {
+            return (Image)(new Bitmap(imgToResize, size));
+        }
+
+        
+
+        private Image CreateImage(string filePath)
+        {
+            ImageSource source = new BitmapImage(new Uri(filePath, UriKind.Relative));
+            Image image = new Image
+            {
+                Source = source,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Margin = new Thickness(5),
+            };
+            // A small rendering tweak to ensure maximum visual appeal.
+            RenderOptions.SetBitmapScalingMode(image, BitmapScalingMode.HighQuality);
+            return image;
+        }
+    }
+
+    internal class Bitmap : Image
+    {
+        private Image imgToResize;
+        private Size size;
+
+        public Bitmap(Image imgToResize, Size size)
+        {
+            this.imgToResize = imgToResize;
+            this.size = size;
         }
     }
 }
